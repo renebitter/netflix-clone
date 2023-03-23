b233daba80870f4117608ca9ec99c37c0749da0e
Rene <renebitter@gmail.com>
2023-03-22 23:00:49 -0300

    Add prisma schema

---
+++ lib/prismadb.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export default prisma;
+
+// "global" is not affected by hot-reloading
+// const client = global.prismadb || new PrismaClient();
+
+// if (process.env.NODE_ENV === 'production') global.prismadb = client;
+
+// export default client;
