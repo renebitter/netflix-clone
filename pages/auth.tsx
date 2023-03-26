ae00cddb052588c9716b93d30f86fd1cf562f231
Rene <renebitter@gmail.com>
2023-03-25 22:27:38 -0300

    Add OAuth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,8 @@
 import { useCallback, useState } from 'react';
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/router';
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
 import axios from 'axios';
 import Input from '@/components/Input';
 
@@ -100,6 +102,50 @@ const Auth = () => {
                 transition'>
               {variant === 'login' ? 'Sign in' : 'Register'}
             </button>
+
+            <div
+              className='
+                flex
+                flex-row
+                items-center
+                gap-4
+                mt-8
+                justify-center
+              '>
+              <div
+                onClick={() => signIn('google', { callbackUrl: '/' })}
+                className='
+                  w-10
+                  h-10
+                  bg-white
+                  rounded-full
+                  flex
+                  justify-center
+                  items-center
+                  cursor-pointer
+                  hover:opacity-80
+                  transition
+                '>
+                <FcGoogle size={30} />
+              </div>
+              <div
+                onClick={() => signIn('github', { callbackUrl: '/' })}
+                className='
+                  w-10
+                  h-10
+                  bg-white
+                  rounded-full
+                  flex
+                  justify-center
+                  items-center
+                  cursor-pointer
+                  hover:opacity-80
+                  transition
+                '>
+                <FaGithub size={30} />
+              </div>
+            </div>
+
             <p className='text-neutral-500 mt-12'>
               {variant === 'login'
                 ? 'First time using Netflix?'
