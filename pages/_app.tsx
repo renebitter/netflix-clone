5d4b377468fccbf4a97a2c37d0782ab05fd08674
Rene Bitter <renebitter@gmail.com>
2023-08-25 13:18:17 -0300

    Add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>FLIXNET</title>
+        <meta
+          name='description'
+          content='A clone from a popular video streaming service'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
